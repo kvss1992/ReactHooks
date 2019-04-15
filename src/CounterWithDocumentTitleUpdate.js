cd5fa07237a062b51b072a5d129f2c104f2ec29b
Karanvir Sawhney <kvss1992@users.noreply.github.com>
2019-04-15 13:03:25 -0400

    getting setup

---
+++ src/CounterWithDocumentTitleUpdate.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+
+function CounterWithDocumentTitleUpdate() {
+  const [count, setCount] = useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click here!</button>
+    </div>
+  );
+}
+
+export default CounterWithDocumentTitleUpdate;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import CounterWithDocumentTitleUpdate from "./CounterWithDocumentTitleUpdate";
 
-import "./styles.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-  //  Demo componentDidMount and componentDidUpdate
-  useEffect(() => {
-    document.title = `You clicked ${count} times`;
-  });
-  return (
-    <div>
-      <p>Count {count}</p>
-      <button onClick={() => setCount(count + 1)}> Click here!</button>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <CounterWithDocumentTitleUpdate />;
+  }
 }
 
 const rootElement = document.getElementById("root");
